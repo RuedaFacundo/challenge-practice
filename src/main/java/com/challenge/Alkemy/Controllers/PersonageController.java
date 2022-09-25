1fa1636f743fc0e881c1b383218799fba59aa68d
Facundo <facundoarueda@hotmail.com>
2022-09-25 16:29:39 -0300

    DEV : Add save personage in controller

--- src/main/java/com/challenge/Alkemy/Controllers/PersonageController.java
+++ src/main/java/com/challenge/Alkemy/Controllers/PersonageController.java
@@ -2,12 +2,13 @@ package com.challenge.Alkemy.Controllers;
 
 import com.challenge.Alkemy.Model.Domain.PersonageDTO;
 import com.challenge.Alkemy.Services.PersonageService;
+import com.sun.istack.NotNull;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/personage")
@@ -24,5 +25,14 @@ public class PersonageController {
         return ResponseEntity.ok(
                 personageService.findAll()
         );
+    }
+
+    @PostMapping
+    public Map<String, String> save(@RequestBody @NotNull PersonageDTO dto){
+        PersonageDTO personageDTO = personageService.savePersonage(dto);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Name: ", personageDTO.getName());
+        map.put("message: ", "Saved successfully");
+        return map;
     }
 }
